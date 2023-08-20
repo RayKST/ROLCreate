20ff1f8b3c29dabfac76841f8dd06feb5b29f3fa
Kauan <jailma.kmp@gmail.com>
2023-08-20 15:04:44 -0300

    [feat] new requirement

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 excel = pd.ExcelFile("input/input.xlsx")
 df = excel.parse("Planilha1")
-df = df.sort_values(by="Header Row")
+colunaBase = 'Header Row'#input("Insira o nome da coluna para ser ordenada: ")
+df = df.sort_values(by=colunaBase)
 writer = pd.ExcelWriter('output/output.xlsx')
-df.to_excel(writer,sheet_name='Planilha1',columns=["Header Row"],index=False)
+df.to_excel(writer,sheet_name='Planilha1',columns=[colunaBase],index=False)
 writer.close()
+
+print("Planilha criada")
