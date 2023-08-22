0aa0b5f83ce7d31e54cbdd540c6941b6f89b010a
Kauan <jailma.kmp@gmail.com>
2023-08-21 23:15:29 -0300

    [feat] dataframe sort

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import pandas as pd
+from openpyxl import Workbook
+import string
+
+alfabeto = []
+for letra in string.ascii_uppercase:
+    alfabeto.append(letra)
 
 excel = pd.ExcelFile("input/input.xlsx")
 df = excel.parse("Planilha1")
-colunaBase = 'Header Row'#input("Insira o nome da coluna para ser ordenada: ")
+colunaBase = input("Insira o nome da coluna para ser ordenada: ")
 df = df.sort_values(by=colunaBase)
-writer = pd.ExcelWriter('output/output.xlsx')
-df.to_excel(writer,sheet_name='Planilha1',columns=[colunaBase],index=False)
-writer.close()
 
+
+workbook = Workbook()
+sheet = workbook.active
+
+indexNumeros = 0
+for index, valor in enumerate(df[colunaBase].to_list()):
+    if index%5 == 0:
+        indexNumeros += 1
+        indexLetra = 0
+    
+    cell = alfabeto[indexLetra] + str(indexNumeros)
+    sheet[cell] = valor
+    indexLetra += 1
+
+workbook.save(filename="output/"+ input("Insira o nome da planilha a ser criada: ") +".xlsx")
 print("Planilha criada")
